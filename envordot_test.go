d195cfd5b84d1c04edbef9d5d42115fd3e814438
Massn <massn23579@gmail.com>
2024-02-06 12:14:09 +0900

    add test_param_without_quotation

--- envordot_test.go
+++ envordot_test.go
@@ -9,6 +9,7 @@ import (
 
 const testDotEnvFile string = ".test.env"
 const testParam = "ENVORDOT_TEST_PARAM"
+const testParamWithoutQuotation = "ENVORDOT_TEST_PARAM_WITHOUT_QUOTATION"
 const testDotEnvVal = "test_value_in_dot_env"
 const testEnvVal = "test_value_in_env"
 
@@ -42,6 +43,40 @@ func TestEnvFirstLoad(t *testing.T) {
 	err := Load(false, testDotEnvFile)
 	assert.Nil(t, err)
 	assert.Equal(t, testEnvVal, os.Getenv(testParam))
+}
+
+func TestLoad(t *testing.T) {
+	tests := []struct {
+		name        string
+		dotEnvFirst bool
+		want        map[string]string
+	}{
+		{
+			name:        "DotEnvFirst",
+			dotEnvFirst: true,
+			want: map[string]string{
+				testParam:                 testDotEnvVal,
+				testParamWithoutQuotation: testDotEnvVal,
+			},
+		},
+		{
+			name:        "EnvFirst",
+			dotEnvFirst: false,
+			want: map[string]string{
+				testParam:                 testEnvVal,
+				testParamWithoutQuotation: testDotEnvVal,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setupTest()
+			defer teardownTest()
+			got, err := Read(tt.dotEnvFirst, testDotEnvFile)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
 }
 
 func setupTest() {
